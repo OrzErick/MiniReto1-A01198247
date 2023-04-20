53ffb2a9e712f03222a09b65f7b813a38e124502
OrzErick <114495906+OrzErick@users.noreply.github.com>
2023-04-19 18:56:41 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    alert("Bienvenidos a mi Pagina Retro By: Erick Reys");
+});
+
+function loanImg() {
+    var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'messi.jpg', true);
+  xhr.responseType = 'blob'; // importante indicar que se espera una respuesta de tipo blob
+  xhr.onload = function() {
+    if (this.status == 200) {
+      var imagenBlob = this.response;
+      var urlImagen = URL.createObjectURL(imagenBlob); // crear una URL temporal para la imagen
+      var img = new Image();
+      img.src = urlImagen;
+      document.body.appendChild(img); // agregar la imagen al documento
+    }
+  };
+  xhr.send();
+  }
